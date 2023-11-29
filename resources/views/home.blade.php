09d6d6f766b372fc5f59957112543388265684b2
Felipe Pinheiro <santospinheiro627@gmail.com>
2023-11-28 22:23:38 -0300

    up

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,5 +6,7 @@
     <h1 class="text-center p-2 m-4 text-2xl uppercase">Crud APP v6</h1>
 </div>
 
+{{-- https://youtu.be/rqtZ0EmciJ8?si=sf-Ovbv9o9J78LgZ video aula --}}
+
 @endsection
 
---
+++ resources/views/users.blade.php
@@ -0,0 +1,11 @@
+@extends('master')
+
+@section('content')
+
+<div>
+    <h1 class="text-center p-2 m-4 text-2xl uppercase">Crud APP v6</h1>
+</div>
+
+
+
+@endsection
